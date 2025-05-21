089a00c60e54e560f8112cdd9ca515b2fc0e05be
chandu481 <chandusri481@gmail.com>
2025-05-21 18:34:44 +0530

    bowling score cal full implementation

---
+++ main.cpp
@@ -0,0 +1,189 @@
+
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+void clearCinError()
+{
+    cerr << "Invalid input! Please enter an integer." << endl;
+    
+    // Clear the error flag
+    cin.clear();
+
+    // Discard the invalid input
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int printResult(vector<int>& scores, int round)
+{
+    int total = 0;
+    int lineNo = 1;
+    for(int i=0; i<scores.size(); )
+    {
+        if(lineNo <= 10)
+        cout<<"Round "<<lineNo<<"-----> ";
+        int s1 = scores[i];
+        cout<<s1<<"    ";
+        if(s1 == 10)
+        {
+            if(i+2 < scores.size())
+            {
+                int val1 = scores[i+1];
+                int val2 = scores[i+2];
+                int res = s1+val1 + val2;
+                total += res;
+                    cout<<"  \t "<<total;
+            }
+            i++;
+        }
+        else
+        {
+            if(i+1 < scores.size())
+            {
+                int val1 = scores[i+1];
+                cout<<val1;
+                if(s1 + val1 == 10)
+                {
+                    if(i+2 < scores.size())
+                    {
+                        int val2 = scores[i+2];
+                        int res = s1 + val1 + val2;
+                        total += res;
+                            cout<<"  \t "<<total;
+                    }
+                }
+                else
+                {
+                    total += s1 + val1;
+                        cout<<"  \t "<<total;
+                }
+                
+            }
+            i = i+2;
+        }
+        if(lineNo < 10)
+        {
+            cout<<endl;
+        }
+        lineNo++;
+    }
+    return total;
+}
+
+int main() 
+{
+    vector<int> scores;
+    int total = 0;
+    int s1 = 0, s2 = 0;
+    for(int i=1; i<=10; i++)
+    {
+        
+        cout<<"---------- Round "<<i<<" -------------"<<endl;
+        bool tryAgain = false;
+        do {
+            tryAgain = false;
+            cout<<"Enter try 1 (0-10): ";
+            cin>>s1;
+            if (cin.fail()) 
+            {
+                tryAgain = true;
+                clearCinError();
+            }   
+            else if(!(s1 >= 0 && s1 <= 10) )
+            {
+                cout<<"Invalid input!!"<<endl;
+                tryAgain = true;
+            }
+        }while(tryAgain);
+        
+        scores.push_back(s1);
+        int range = 10-s1;
+        total = printResult(scores, i);
+        if(s1 != 10 || i == 10)
+        {
+            if(i == 10)
+            {
+                bool tryAgain = false;
+                do {
+                    tryAgain = false;
+                    cout<<endl;
+                    cout<<"Enter try 2 (0-10): ";
+                    cin>>s2;
+                    if (cin.fail()) 
+                    {
+                        tryAgain = true;
+                        clearCinError();
+                    }      
+                    else if(!(s2 >= 0 && s2 <= 10) )
+                    {
+                        cout<<"Invalid input!!"<<endl;
+                        tryAgain = true;
+                    }
+                }while(tryAgain);
+                
+            }
+            else 
+            {
+                bool tryAgain = false;
+                do {
+                    tryAgain = false;
+                    //cout<<endl;
+                    cout<<"Enter try 2 (0-"<<range<<"): ";
+                    cin>>s2;
+                    if (cin.fail()) 
+                    {
+                        tryAgain = true;
+                        clearCinError();
+                    }   
+                    else if(!(s2 >= 0 && s2 <= range) )
+                    {
+                        cout<<"Invalid input!!"<<endl;
+                        tryAgain = true;
+                    }
+                }while(tryAgain);
+            }
+
+            
+            scores.push_back(s2);
+            total = printResult(scores, i);
+            
+            if(i == 10)
+            {
+                if(s1 + s2 >= 10)
+                {
+                    total += s1 + s2;
+                    int extraPoint;
+                    
+                    bool tryAgain = false;
+                    do {
+                        tryAgain = false;
+                        //cout<<endl;
+                        cout<<"\nExtra chance (0-10): ";
+                        cin>>extraPoint;
+                        
+                        if (cin.fail()) 
+                        {
+                            tryAgain = true;
+                            clearCinError();
+                        }   
+                        else if(!(extraPoint >= 0 && extraPoint <= range) )
+                        {
+                            cout<<"Invalid input!!"<<endl;
+                            tryAgain = true;
+                        }
+                    }while(tryAgain);
+
+                    total += extraPoint;
+                    printResult(scores,i);
+                    cout<<"\n\t\t ::"<<extraPoint;
+                    cout<<"    "<<total;
+                }
+                
+            }
+        }
+    }
+    
+    return 0;
+}
